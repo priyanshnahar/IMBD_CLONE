4a4599f09a1bc61db6837783b216289adfa8e957
Priyansh Nahar <priyanshnahar1998@gmail.com>
2023-03-16 23:13:04 +0530

    Adding header

--- client/src/component/common/Header.jsx
+++ client/src/component/common/Header.jsx
@@ -1,11 +1,33 @@
-import {AppBar, Toolbar} from '@mui/material';
+import {AppBar, Toolbar, styled, Typography} from '@mui/material';
+import { logoURL } from '../../constants/constant';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box } from '@mui/system';
 
+const StyledToolBar = styled(Toolbar) `
+    background: #121212;
+    min-height: 56px !important;
+    padding: 0 115px !important;
+
+`
+
+const Logo = styled("img") ({
+    width: 64
+})
 
 const Header = () => {
     return (
-        <div>
-            hello from header
-        </div>
+     <AppBar>
+        <StyledToolBar>
+        <Logo src={logoURL} alt="logo" />
+        <Box>
+        <MenuIcon/>
+        <Typography>
+        Menu
+      </Typography>
+        </Box>
+     
+        </StyledToolBar>
+     </AppBar>
     )
 }
 
