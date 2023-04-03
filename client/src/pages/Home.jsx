6a0678078f3d267c478dfeb13da84e09966a07b6
Priyansh Nahar <priyanshnahar1998@gmail.com>
2023-04-03 15:49:14 +0530

    Adding files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,21 +2,27 @@ import { useEffect, useState } from "react";
 
 import Header from "../component/common/Header";
 import { categoryMovies } from "../services/api";
+import { NOWPLAYING_API_URL } from "../constants/constant";
+import { Box } from "@mui/material";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([])
-
-    useEffect( () => {
-        const getData = async () => {
-            let response = await categoryMovies();
-            setMovies()
-        }
+  useEffect(() => {
+    const getData = async () => {
+      let response = await categoryMovies(NOWPLAYING_API_URL);
+      setMovies(response.results);
+    };
     getData();
-    }, [])
-    return (
-            <div><Header/></div>
-    )
-}
+  }, []);
+  return (
+    <>
+      <Header />
+      <Box>
+        <Banner />
+      </Box>
+    </>
+  );
+};
 
 export default Home;
